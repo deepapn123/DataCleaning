1435a2e6f252347dcae4668f0aa0ba1b062e7fda
deepapn123 <deepapraveenmail@gmail.com>
2025-09-21 19:45:12 +0530

    Add files via upload

---
+++ cl.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+def isnumeric_ornull(value):
+    try:
+        if(float(value) or value==""):
+            return True
+    except (ValueError, TypeError):
+        return False
+    
+# Create a simple DataFrame
+
+df = pd.read_csv('ms.csv')
+
+print("\n Original Data Frame: \n")
+print(df)
+
+# checking for invalid data for column 'NAME' and then removing it 
+
+
+for value,x in zip(df['NAME'],df.index):
+    if isnumeric_ornull(value):
+        df=df.drop(x)
+print("\n\n After cleaning NAME field\n")       
+print(df)
+    
+    
+           
+     
+
